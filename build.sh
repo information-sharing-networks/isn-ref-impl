b0fc320a9c9b0f135ed158aefe297735b92f4a4f
Nick Abson <4947488+nick-abson@users.noreply.github.com>
2024-07-04 07:22:47 +0100

    build.sh updates and minor version increased to 0.11.0

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #/bin/bash
 
 function usage(){
-    echo "usage:  $0 [ -t (run tests) ] -b current|patch|minor" 2>&1
+    echo "usage:  $0 -b current|patch|minor [ -t ] 
+    -t (run tests)
+
+    creates a tar archive of the system files needed to install a new ISN site 
+
+    EXAMPLE
+    to build a new minor version  (this will create a new archive in the build dir and increment the minor version in version.edn)
+    $0 -b minor
+    " 2>&1
 	exit 1
 }
 
@@ -9,11 +17,12 @@ while getopts "tb:" arg; do
   case $arg in
     b) export BUILD_TYPE=$OPTARG ;;
     t) export RUN_TESTS=1;;
+    *) usage;;
   esac
 done
 
 if [ -z "$BUILD_TYPE" ];then
-    echo '-b (build type) is mandatory' 2>&1
+    echo '-b is required' 2>&1
     usage
 fi
 if [ "$BUILD_TYPE" != "current" ] && [ "$BUILD_TYPE" != "patch" ] && [ "$BUILD_TYPE" != "minor" ]; then 
@@ -34,7 +43,7 @@ echo creating build for $BUILD_TYPE version
 clj -T:build $BUILD_TYPE
 
 echo creating tar file
-tar czf isn.tgz config config.template.edn deps.edn README.md resources scripts src START.template STOP.template version.edn
+tar czf isn.tgz config templates deps.edn README.md resources src version.edn
 
 v=$(cat version.edn |gawk 'match($0,/.*([0-9]+.[0-9]+.[0-9]+).*/,a) { print a[1] } ')
 echo $v  debug
